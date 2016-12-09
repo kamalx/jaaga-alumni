b39296b33786188e37648ea0910d219f6ed57162
Kamaleshwar Morjal <kamal@morjal.com>
2016-12-09 11:42:47 +0530

    Move stubbed profile data into separate file

--- site/src/App.js
+++ site/src/App.js
@@ -5,6 +5,8 @@ import ProfileHeader from './components/ProfileHeader';
 import Skills from './components/Skills';
 import './App.css';
 
+import { profile_data } from './mock_profile_data';
+
 class App extends Component {
   constructor() {
     super();
@@ -12,42 +14,7 @@ class App extends Component {
     // define initial state
     // status may be one of { 'current', 'alumni', 'applied', 'advisor' }
     this.state = {
-      profile: {
-        status: 'current',
-        firstname: 'Kamal',
-        lastname: 'Morjal',
-        dob: '1984-02-13',
-        email: 'kamal@morjal.com',
-        twitter: 'kamalx',
-        facebook: 'kamalx',
-        github: 'kamalx',
-        address: 'Hamsah Organic Farm',
-        city: 'Bengaluru',
-        forHire: true,
-        skills: {
-          programming: [
-            { name: "Javascript", initial: 50, current: 80 },
-            { name: "Java", initial: 2, current: 30 },
-            { name: "Clojure", initial: 5, current: 7 },
-            { name: "HTML5", initial: 5, current: 7 }
-          ],
-          graphic_design_tools: [
-            { name: "Inkscape", initial: 80, current: 85 },
-            { name: "GIMP", initial: 75, current: 75 },
-            { name: "Blender", initial: 40, current: 45 },
-            { name: "Adobe Photoshop", initial: 40, current: 40 },
-            { name: "Adobe Illustrator", initial: 40, current: 40 }
-          ],
-          other_skills: [
-            { name: "Cooking", initial: 45, current: 65 },
-            { name: "Unicycling", initial: 0, current: 55 },
-            { name: "Welding", initial: 15, current: 20 },
-            { name: "Soldering", initial: 15, current: 20 },
-            { name: "Electric Wiring", initial: 15, current: 20 }
-          ]
-        }
-      }
-
+      profile: profile_data
     }
   }
 
---
+++ site/src/mock_profile_data.js
@@ -0,0 +1,36 @@
+export const profile_data = {
+  status: 'current',
+  firstname: 'Kamal',
+  lastname: 'Morjal',
+  dob: '1984-02-13',
+  email: 'kamal@morjal.com',
+  twitter: 'kamalx',
+  facebook: 'kamalx',
+  github: 'kamalx',
+  address: 'Hamsah Organic Farm',
+  city: 'Bengaluru',
+  forHire: true,
+  skills: {
+    programming: [
+      { name: "Javascript", initial: 50, current: 70 },
+      { name: "Java", initial: 2, current: 30 },
+      { name: "Clojure", initial: 5, current: 7 },
+      { name: "HTML5", initial: 75, current: 80 },
+      { name: "CSS3", initial: 75, current: 80 },
+    ],
+    graphic_design_tools: [
+      { name: "Inkscape", initial: 80, current: 85 },
+      { name: "GIMP", initial: 75, current: 75 },
+      { name: "Blender", initial: 40, current: 45 },
+      { name: "Adobe Photoshop", initial: 40, current: 40 },
+      { name: "Adobe Illustrator", initial: 40, current: 40 }
+    ],
+    other_skills: [
+      { name: "Cooking", initial: 45, current: 65 },
+      { name: "Unicycling", initial: 0, current: 55 },
+      { name: "Welding", initial: 15, current: 20 },
+      { name: "Soldering", initial: 15, current: 20 },
+      { name: "Electric Wiring", initial: 15, current: 20 }
+    ]
+  }
+}
